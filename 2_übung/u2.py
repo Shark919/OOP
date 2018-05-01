c26f76c3acb27bb09a7d1ab5002257e1f0a9ff68
Tim <t.walz@freenet.de>
2018-05-01 11:57:05 +0200

    update

--- 2_übung/u2.py
+++ 2_übung/u2.py
@@ -1,8 +1,8 @@
-#import math
-#import operator
+import math
 import time
 import random
 
+# 1. Aufgabe
 def apply_if(f, p, xs):
     # assumes f, p is a function and xs is a list
     res = []
@@ -13,20 +13,54 @@ def apply_if(f, p, xs):
             res.append(x)
     return res
 
+# Hilfsfunktion für 1. Aufgabe
 def odd(x):
     if x % 2 == 1:
         return True
     else:
         return False
 
-# Rekursion fehlt noch
+# Testmethode der 1. Aufgabe
+def test_apply_if():
+    if apply_if(math.factorial, odd, [2,5,7,4,9,6]) == [2, 120, 5040, 4, 362880, 6]:
+        print("Test bestanden")
+    else:
+        print("Test nicht bestanden")
 
+# 2. Aufgabe Teil a
+def zipWith(f, xs, ys):
+    # assumes xs, ys are lists
+    if len(xs) == 1 or len(ys) == 1:
+        return [f(xs[0], ys[0])]
+    else:
+        return [f(xs[0], ys[0])] + zipWith(f, xs[1:], ys[1:])
+
+# 2. Aufgabe Teil b
 def zipWith2(f, xs, ys):
-    # assumes xs, ys is a list
+    # assumes xs, ys are lists
     res = [f(a,b) for (a,b) in zip(xs, ys)]
     return res
 
-def myRandom(lower, upper):
+# Testmethode der 2. Aufgabe inkl. Teil c
+def test_zipWith():
+    start = time.time()
+    test1 = zipWith(divmod, [1,2,3,15], [1,2,3,4])
+    end = time.time()
+    if test1 == [(1, 0), (1, 0), (1, 0), (3, 3)]:
+        print("Test zipWith bestanden, die Ausführungszeit betrug:", (end-start))
+    else:
+        print("Test zipWith nicht bestanden, die Ausführungszeit betrug:", (end-start))
+
+    start = time.time()
+    test2 = zipWith2(divmod, [1,2,3,15], [1,2,3,4])
+    end = time.time()
+    if test2 == [(1, 0), (1, 0), (1, 0), (3, 3)]:
+        print("Test zipWith2 bestanden, die Ausführungszeit betrug:", (end-start))
+    else:
+        print("Test zipWith2 nicht bestanden, die Ausführungszeit betrug:", (end-start))
+
+# 3. Aufgabe
+def my_random(lower, upper):
     i = 0
     dictionary = {}
     while str(random.randint(lower, upper)) not in dictionary:
@@ -34,6 +68,16 @@ def myRandom(lower, upper):
         i += 1
     return i
 
+# Testmethode der 3. Aufgabe
+def test_my_random():
+    lower = -100
+    upper = 100
+    if my_random(lower, upper) > 0:
+        print("Test my_random bestanden")
+    else:
+        print("Test my_random nicht bestanden")
+
+# 4. Aufgabe Teil a
 def double_birthday():
     i = 1
     birthdays = {}
@@ -44,25 +88,28 @@ def double_birthday():
         birthday = createBD()
     return i
 
+# Hilfsfunktion zum generieren von Geburtstagen für Teil a
 def createBD():
     year = random.randint(1940, 2017)
     month = random.randint(1, 12)
     day = random.randint(1, 28)
     return str(day).zfill(2) + str(month).zfill(2) + str(year)
 
+# Aufgabe 4 Teil b
 def repeat_double_birthday():
-    duplicates = {}
+    duplicates = []
     i = 0
-    while i < 365:
-        num = str(double_birthday())
-        if num not in duplicates:
-            duplicates[num] = 1
-        else:
-            duplicates[num] += 1
-        i +=1 
+    while i < 1000:
+        bd = double_birthday()
+        if bd > 365:
+            pass
+        else: 
+            duplicates.append(bd)
+        i +=1
     return duplicates
 
-# stimmt noch nicht
+# Aufgabe 4 Teil c
+# stimmt aber nicht
 #def birthday_paradox(n):
 #    duplicates = repeat_double_birthday()
 #    if str(n) in duplicates:

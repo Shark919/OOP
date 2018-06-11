fae5634e204d67b5525ace4afb8ae6ca66aceea5
Tim <t.walz@freenet.de>
2018-06-11 13:51:29 +0200

    Final Commit

--- 6_übung/src/Rectangle.java
+++ 6_übung/src/Rectangle.java
@@ -1,3 +1,5 @@
+import javax.xml.ws.AsyncHandler;
+
 import org.omg.CosNaming._BindingIteratorImplBase;
 
 /**
@@ -110,16 +112,30 @@ public class Rectangle {
 		}
 		return result;
 	}
-
+	/**
+	 * Berechnet die rechteckige Schnittfläche, die entsteht, wenn ein Rectangle-Objekt 
+	 * sich mit einem zweiten Rectangle-Objekt überlappt. Wenn die Rectangle-Objekte 
+	 * sich nicht überlappen, wird die Konstante null zurückgegeben
+	 * @param r
+	 * @return new Rectangle
+	 */
 	public Rectangle section (Rectangle r) {
 		
-		if (this.overlaps(r)) {
-			System.out.println("1");
-			return new Rectangle();
+		 if (this.contains(r)) {
+			return r;
+		} else if (r.contains(this)) {
+			return this;
+		} else if (this.overlaps(r)) {
+			Rectangle sec = new Rectangle();
+			sec.x = Math.max(this.x, r.x);
+			sec.width =	Math.min(this.x + this.width, r.x + r.width) - sec.x;
+			sec.y = Math.max(this.y, r.y);
+			sec.height = Math.min(this.y + this.height, r.y + r.height) - sec.y;		
+			return sec;
 		} else {
-			System.out.println("3");
 			return null;
 		}
+		
 	}
 	
 	/**
@@ -150,29 +166,20 @@ public class Rectangle {
 		return new Rectangle(minX, minY, maxWidth-minX, maxHeight-minY);
 	}
 	
+	// Die Klasse TestRectangle setzt die Methoden getX(), getY(), getWidth() und getHeight() vorraus.
+	// Diese werden zwar nicht in der Aufgabenstellung gefordert, wurden hier jedoch trotzdem implementiert,
+	// um ein Testen mit der Klasse zu ermöglichen.
 	public int getX() {
 		return this.x;
-	}
-	public void setX(int x) {
-		this.x = x;
 	}
 	public int getY() {
 		return this.y;
 	}
-	public void setY(int y) {
-		this.y = y;
-	}
 	public int getWidth() {
 		return this.width;
 	}
-	public void setWidth(int width) {
-		this.width = width;
-	}
 	public int getHeight() {
 		return this.height;
-	}
-	public void setHeight(int height) {
-		this.height = height;
 	}
  }
 
--- 6_übung/src/SomeCalculations.java
+++ 6_übung/src/SomeCalculations.java
@@ -1,7 +1,20 @@
 import java.util.Arrays;
 
+/**
+ * 
+ * @author Christop Meise, Tim Walz
+ * @version 1.0
+ * OOP Tutorium Freitag 08-10:00 Uhr
+ *
+ */
+
 public class SomeCalculations {
 
+	/**
+	 * Rekursive Berechnung der Quersumme von ganzen Zahlen
+	 * @param number as integer value
+	 * @return Quersumme 
+	 */
 	public static int querSumme(int number) {
 		if (number < 10) {
 			return number;
@@ -10,18 +23,27 @@ public class SomeCalculations {
 		}
 	}
 	
+	/**
+	 * Berechnet ob die Querumme einer Zahl und die Zahl selber
+	 * durch drei teilbar ist
+	 * @param number as integer value
+	 * @return true | false
+	 */
 	public static boolean multipleOf3(int number) {
 		Boolean isMultipleOf3 = false;
-		if (querSumme(number) % 3 == 0 && number % 3 == 0) {
+		if (querSumme(number) % 3 == 0 & number % 3 == 0 & number != 0) {
 			return true;
 		}
 		return isMultipleOf3;
 	}
 	
+	// Hinweis: Wir verwenden assertions zum Testen unserer Funktionen. Um diese in Eclipse
+	// zu Berücksichtigen, muss in der Run-Cofiguration -> Arguments -> VM Arguments '-ea' als
+	// Wert gesetzt sein.
 	public static void main(String[] args) {
 		int[] testArray = {0,1,2,3,5,10,16,84,88,12564};
-		int[] solvedArrayQuersumme = {0,1,2,3,5,1,7,12,16,20};
-		boolean[] solvedArrayMultipleOf3 = {false, false, false, true, false, false, true, false, false};
+		int[] solvedArrayQuersumme = {0,1,2,3,5,1,7,12,16,18};
+		boolean[] solvedArrayMultipleOf3 = {false, false, false, true, false, false, false, true, false, true};
 		
 		System.out.println("Der Test startet mit folgenden Werten:");
 		System.out.println("Getestete Werte: " + Arrays.toString(testArray));
@@ -29,7 +51,7 @@ public class SomeCalculations {
 		System.out.println("Erwartete Werte für multipleOf3() sind: " + Arrays.toString(solvedArrayMultipleOf3));
 		
 		for (int i = 0; i < testArray.length; i++) {
-			assert querSumme(testArray[i]) == solvedArrayQuersumme[i] : "Die Quersumme von " + solvedArrayQuersumme[i] + " wurde mit " + querSumme(testArray[i]) + " falsch berechnet";
+			assert querSumme(testArray[i]) == solvedArrayQuersumme[i] : "Die Quersumme von " + testArray[i] + " wurde mit " + querSumme(testArray[i]) + " falsch berechnet";
 			assert multipleOf3(testArray[i]) == solvedArrayMultipleOf3[i] : "Die Methode multipleOf3() lieferte für " + testArray[i] + " das Falsche Ergebnis"; 
 		}
 		
